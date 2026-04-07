4ebea21c1a176d898341384b335f3264cad94e16
edmounds <edmounds@163.com>
2026-04-07 16:59:29 +0800

    update:整合livox_ros_driver2的launch文件到fast-lio启动配置中

--- launch/fast-lio.launch.py
+++ launch/fast-lio.launch.py
@@ -5,23 +5,23 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    r2_spawner_path = get_package_share_directory('r2_spawner')
-    joy_conventor_path = get_package_share_directory('joy_linux')
-
     fast_lio_path = get_package_share_directory('fast_lio')
+    livox_driver_path = get_package_share_directory('livox_ros_driver2')
 
     fast_lio_rviz_config_path = os.path.join(
         fast_lio_path, 'rviz', 'fastlio.rviz')
 
     fast_lio_config_file = os.path.join(
         fast_lio_path, 'config', 'mid360.yaml'
+    )
+    livox_driver_launch_path = os.path.join(
+        livox_driver_path, 'launch_ROS2', 'msg_MID360_launch.py'
     )
 
     use_sim_time = LaunchConfiguration('use_sim_time')
@@ -47,9 +47,14 @@ def generate_launch_description():
         parameters=[{'use_sim_time': use_sim_time}]
     )
 
+    livox_driver_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(livox_driver_launch_path)
+    )
+
     ld = LaunchDescription()
 
     ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(livox_driver_launch)
     ld.add_action(fast_lio_node)
     ld.add_action(rviz_node)
 
